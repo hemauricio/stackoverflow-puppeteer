05061534ce0ffd5f19969409649d4c9e74175648
mmhernandez <mauriciohdez08@gmail.com>
2020-06-26 21:54:30 -0500

    Set headless to true

--- src/stackoverflow.js
+++ src/stackoverflow.js
@@ -5,13 +5,16 @@ const config = require("../secrets/secrets.js");
 const puppeteer = require("puppeteer");
 
 async function login(){
+
+  console.log("Starting: ", (new Date).toString());
+
   /*
   I'm setting headless to false to visualize the process. It can be removed.
   It's very important to configure the slowMo, as it's not only useful
   for you to see what is going on, but also to not trigger the anti-abuse system,
   that way, human intervention won't be neccessary.
   */
-  const browser = await puppeteer.launch({headless: false, slowMo: 50});
+  const browser = await puppeteer.launch({headless: true, slowMo: 50});
 
   const page = await browser.newPage();
 
@@ -37,8 +40,10 @@ async function login(){
 
   // Waits 1.5 seconds to appreciate the result
   await page.waitFor(1500);
-  
+
   await browser.close();
+
+  console.log("Finishing: ", (new Date).toString());
 }
 
 login();
